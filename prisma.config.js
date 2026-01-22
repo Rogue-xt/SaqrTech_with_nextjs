27f6f539c5015521dc9c70c4bc4e9c82ae503348
Rogue_xt <rithik.raghunath@gmail.com>
2026-01-22 12:29:54 +0400

    test branch push

--- prisma.config.js
+++ prisma.config.js
@@ -4,8 +4,10 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
-    // We use process.env directly here to ensure it's captured
-    url: process.env.DATABASE_URL,
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+      directUrl: process.env.DIRECT_URL,
+    },
   },
 });
